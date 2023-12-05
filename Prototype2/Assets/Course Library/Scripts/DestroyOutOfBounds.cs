de791bae76d8219aef141a899cf11ee2c65875fd
Prince Robinson <robinsonp@roverkids.org>
2023-12-05 14:20:21 -0500

    L 2.2 done

--- Prototype2/Assets/Course Library/Scripts/DestroyOutOfBounds.cs
+++ Prototype2/Assets/Course Library/Scripts/DestroyOutOfBounds.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class DestroyOutOfBounds : MonoBehaviour
 {
     private float topBound = 30;
+    private float lowerBound = -10;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,7 +18,8 @@ public class DestroyOutOfBounds : MonoBehaviour
     {
         if (transform.position.z > topBound)
         {
-            Destroy(gameObject);
-        }
-    }
+        Destroy(gameObject);
+      } else if (transform.position.z < lowerBound) {
+        Destroy(gameObject);
+        } }
 }
