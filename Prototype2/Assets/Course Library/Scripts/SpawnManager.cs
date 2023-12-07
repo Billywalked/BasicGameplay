5a3e33aeb7eb2f3a4d8f1ede04ca69473247cbba
Prince Robinson <robinsonp@roverkids.org>
2023-12-07 14:14:04 -0500

    L 2.2 step 4

--- Prototype2/Assets/Course Library/Scripts/SpawnManager.cs
+++ Prototype2/Assets/Course Library/Scripts/SpawnManager.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] animalPrefabs;
-    
+    private float spawnRangeX = 20;
+    private float spawnPosZ = 20;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,8 +18,10 @@ public class SpawnManager : MonoBehaviour
     void Update()
     { 
         if (Input.GetKeyDown(KeyCode.S)) {
+            // Randomly generate animal index spawn position
+            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
             int animalIndex = Random.Range(0, animalPrefabs.Length);
-            Instantiate(animalPrefabs[animalIndex], new Vector3(0, 0, 20),
+            Instantiate(animalPrefabs[animalIndex], spawnPos,
                 animalPrefabs[animalIndex].transform.rotation);
 
 
