e343d7c2afe5d60ad280cc44b956830a4fd9ab21
Prince Robinson <robinsonp@roverkids.org>
2023-12-11 13:23:51 -0500

    2.4 step 1 done

--- Prototype2/Assets/Course Library/Scripts/SpawnManager.cs
+++ Prototype2/Assets/Course Library/Scripts/SpawnManager.cs
@@ -17,15 +17,20 @@ public class SpawnManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     { 
-        if (Input.GetKeyDown(KeyCode.S)) {
+        if (Input.GetKeyDown(KeyCode.S)) 
+        {
             // Randomly generate animal index spawn position
-            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
-            int animalIndex = Random.Range(0, animalPrefabs.Length);
-            Instantiate(animalPrefabs[animalIndex], spawnPos,
-                animalPrefabs[animalIndex].transform.rotation);
+            SpawnRandomAnimal();
 
 
         }
         
+    }
+    void SpawnRandomAnimal()
+    {
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Instantiate(animalPrefabs[animalIndex], spawnPos,
+            animalPrefabs[animalIndex].transform.rotation);
     }
 }
